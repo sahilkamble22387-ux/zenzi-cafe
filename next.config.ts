d30b32f224a474fd1d4a7226b2b440bdaf3b03ea
Sahil Kamble <sahilkamble22387@gmail.com>
2026-05-31 13:51:37 +0530

    feat: setup github pages static export deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  output: "export",
+  basePath: "/zenzi-cafe",
+  assetPrefix: "/zenzi-cafe",
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   reactStrictMode: false,
 };
